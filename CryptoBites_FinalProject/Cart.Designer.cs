4320ea67ccd236c56b87b63b5388faa30c3af9ed
hed-dsanchez-dev <hed-dsanchez@smu.edu.ph>
2025-12-09 14:27:58 +0800

    haha

--- CryptoBites_FinalProject/Cart.Designer.cs
+++ CryptoBites_FinalProject/Cart.Designer.cs
@@ -291,7 +291,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(539, 112);
+            this.textBox1.Location = new System.Drawing.Point(550, 90);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(365, 22);
